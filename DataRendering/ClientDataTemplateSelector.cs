c3211dec45d1a7aa40685b90ff54418a33833980
churkinv <churkinv@gmail.com>
2017-12-01 20:12:27 +0200

    DataRendering: moving DataTemplate to App  + implementation of DataTemplateSelector for conditional switching between templates

---
+++ DataRendering/ClientDataTemplateSelector.cs
@@ -0,0 +1,20 @@
+﻿using DataRendering.Data;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace DataRendering
+{
+    public class ClientDataTemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            var company = (Company)item;
+            var companyTemplate = (DataTemplate)App.Current.Resources["MyTemplate"];
+            var companyTemplatePrivate = (DataTemplate)App.Current.Resources["MyTemplatePrivate"];
+
+            if (company.Name == "samsung") return companyTemplatePrivate;
+
+            return companyTemplate;
+        }
+    }
+}
