5528037cccf4083551973f1ecbc3632db1828e6e
churkinv <churkinv@gmail.com>
2017-12-17 22:36:55 +0200

    WinRT`s data controls: Incremental loading

---
+++ IncrementalLoading/Client.cs
@@ -0,0 +1,30 @@
+﻿using Windows.UI.Xaml.Media;
+
+namespace IncrementalLoading
+{
+    public class Client : Observable
+    {
+        private string _name;
+        private ImageSource _imageSource;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ImageSource ImageSource
+        {
+            get { return _imageSource; }
+            set
+            {
+                _imageSource = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ IncrementalLoading/ClientIncrementalCollection.cs
@@ -0,0 +1,39 @@
+﻿using IncrementalLoading;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml.Data;
+
+namespace IncrementalLoading
+{
+    public class ClientIncrementalCollection : ObservableCollection<Client>, ISupportIncrementalLoading
+    {
+        const uint totalClientOnServer = 100; // faking the server)
+
+        public bool HasMoreItems => this.Count < totalClientOnServer; // true if Count less than on the server 
+
+        public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count) // count contains the count of the elements are requested by the listview
+        {
+            uint unloadedClientCount = totalClientOnServer - (uint)this.Count; // count client that has not been loaded yet
+            uint clientsToLoadCount = Math.Min(unloadedClientCount, count);// to know real number of clients to load we need to take minimum value out of the count and out of the unloadedClientCount
+            return LoadMoreItemsInternalAsync(clientsToLoadCount).AsAsyncOperation(); // as we return Task from the pointed method and here we have to return IAsync op. we use .AsAsyncOperation()
+        }
+
+        private async Task<LoadMoreItemsResult> LoadMoreItemsInternalAsync(uint clientsToLoadCount)
+        { 
+            // this is fake server implementation, in real use loading from server
+
+            for (int i = 0; i < clientsToLoadCount; i++)
+            {
+                await Task.Delay(100);
+
+                this.Add(new Client { Name = "Client # " + (this.Count + 1) });
+            }
+            return new LoadMoreItemsResult { Count = clientsToLoadCount };
+        }
+    }
+}
